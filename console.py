b3e2bc9236a82a349049544b5748ba056c4a661e
carrliitos <salazar.ten80@gmail.com>
2020-11-10 21:42:50 -0600

    added more stuff

--- console.py
+++ console.py
@@ -14,3 +14,112 @@ from models.city import City
 from models.baseModel import BaseModel
 from models.engine.fileStorage import FileStorage
 
+nameOfClass = {
+	"BaseModel": BaseModel,
+	"User": User,
+	"State": State,
+	"City": City,
+	"Amenity": Amenity,
+	"Place": Place,
+	"Review": Review,
+}
+
+class ABNBCommand(cmd.Cmd):
+	'''Create an interpreter command in Python'''
+	prompt = "(ConsoleBNB)"
+
+	def doEOF(self, line):
+		'''Exit the program'''
+		return True
+
+	def doQuit(self, line):
+		'''Quit command exit the program\n'''
+		return True
+
+	def emptyLine(self, args):
+		'''Nothing is executed'''
+		pass
+
+	def doCreate(self, args):
+		'''Create a new instance of the class BaseModel'''
+		args = shlex.split(args)
+		if not args:
+			print("**Class name missing**")
+		elif not args[0] in nameOfClass:
+			print("** Class doesn't exist **")
+		else:
+			newObj = eval(args[0])()
+			newObj.save()
+			print(newObj.id)
+
+	def doShow(self, args):
+		'''Prints the string representation of the instance'''
+		args = shlex.split(args)
+		dicti = storage.all()
+		if not args:
+			print("** class name missing **")
+		elif not args[0] in nameOfClass:
+			print("** class doesn't exist **")
+		elif len(args) == 1:
+			print("** instance id missing **")
+		elif "{}.{}".format(args[0], args[1]) in dicti:
+			print(dicti["{}.{}".format(args[0], args[1])])
+		else:
+			print("** no instance found **")
+
+	def doDestroy(self, args):
+		'''Deletes an instance base on the class name and id'''
+		args = shlex.split(args)
+		dicti = storage.all()
+		if not args:
+			print("** class name missing **")
+		elif not args[0] in nameOfClass:
+			print("** class doesn't exist **")
+		elif len(args) == 1:
+			print("** instance id missing **")
+		elif "{}.{}".format(args[0], args[1]) in dicti:
+			dicti.pop("{}.{}".format(args[0], args[1]))
+			storage.save()
+		else:
+			print("** no instance found **")
+
+	def doAll(self, args):
+		'''Prints all strings representation of all instances'''
+		args = shlex.split(args)
+		dicti = storage.all()
+		printall = []
+		if not args:
+			for i in dicti.values():
+				printall.append(str(i))
+			print(printall)
+		elif args[0] in nameOfClass:
+			for key, val in dicti.items():
+				if val.__class__.__name__ == args[0]:
+					printall.append(val.__str__())
+			print(printall)
+		else:
+			print("** class doesn't exist **")
+
+	def doUpdate(self, args):
+		'''Updates an instance base on the class name and id'''
+		args = shlex.split(args)
+		dicti = storage.all()
+		if not args:
+			print("** class name missing **")
+		elif not args[0] in nameOfClass:
+			print("** class doesn't exist **")
+		elif len(args) == 1:
+			print("** instance id missing **")
+		elif not "{}.{}".format(args[0], args[1]) in dicti:
+			print("** no instance found **")
+		elif len(args) == 2:
+			print("** attribute name missing **")
+		elif len(args) == 3:
+			print("** value missing **")
+		else:
+			key = dicti["{}.{}".format(args[0], args[1])]
+			setattr(key, args[2], args[3])
+			key.save()
+
+if __name__ == '__main__':
+	ABNBCommand().cmdloop()
---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from models.engine.fileStorage import FileStorage
+
+storage = FileStorage()
+storage.reload()
