7a42951bbaa3a0750457545639a49782f30c1d3f
carrliitos <salazar.ten80@gmail.com>
2020-11-13 19:19:31 -0600

    added more test cases for different methods

--- tests/testModels/test_city.py
+++ tests/testModels/test_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 """Test City"""
-
 from models.city import City
 from models import storage
 import unittest
@@ -10,12 +9,37 @@ import time
 import os
 import json
 
+# Global variable
 base = City()
 
-class Test_City(object):
+class Test_City(unittest.TestCase):
 	"""docstring for Test_City class"""
 	def test_attr(self):
 		base.name = "Salazar"
 		base.number = 52000
 
 		self.assertAlmostEqual(base.name, "Salazar")
+		self.assertAlmostEqual(base.number, 52000)
+		self.assertEqual(type(base.id), str)
+		self.assertEqual(type(base.createdAt), datetime.datetime)
+		self.assertEqual(type(base.updatedAt), datetime.datetime)
+
+	def test_type(self):
+		"""Tests type class"""
+		self.assertAlmostEqual(type(base), City)
+
+	def test_updated_at(self):
+		"""Tests updatedAt"""
+		create = str(base.createdAt)
+		start = str(base.updatedAt)
+		base.name = "Salazar"
+		base.save()
+
+		self.assertNotEqual(str(base.updatedAt), start)
+		self.assertEqual(str(base.createdAt), create)
+
+	def test_toDict(self):
+		"""Tests dictionary"""
+		base2 = base.toDict()
+
+		self.assertEqual(base2["updatedAt"], base.updatedAt.isoformat())
